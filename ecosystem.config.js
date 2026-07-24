34dc1901ee3d9150bfb30c4490e576d3fe8d2fb8
alexanderbultynck <alexander.bultynck@student.vives.be>
2026-07-24 23:36:06 +0200

    fix: correct script path and ensure proper working directory in ecosystem configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,14 @@ module.exports = {
   apps: [
     {
       name: "ladeco-it-admin",
-      script: "node_modules/next/dist/bin/next",
+      script: "./node_modules/next/dist/bin/next",
       args: "start",
+      cwd: "C:\\Users\\Alexander\\admin-web",
       env: {
         PORT: 3000,
         HOSTNAME: "0.0.0.0",
-        NODE_ENV: "production",
-      },
-    },
-  ],
+        NODE_ENV: "production"
+      }
+    }
+  ]
 };
